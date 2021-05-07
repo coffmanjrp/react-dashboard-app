3cc5c922ff62f7bcded97318e0056dfc7c7233f9
coffmanjrp <coffmanjrp@gmail.com>
2021-05-06 19:58:55 -0700

    Add greetings before the clock loaded

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -7,12 +7,19 @@ const useStyles = makeStyles((theme) => ({
   container: {
     display: 'flex',
     flexDirection: 'column',
+    zIndex: 1,
+  },
+  greeting: {
+    fontSize: '8rem',
+    lineHeight: 1,
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '5rem',
+    },
   },
   time: {
     fontSize: '8rem',
-    height: '70%',
     [theme.breakpoints.down('sm')]: {
-      fontSize: '5rem',
+      fontSize: '4.5rem',
     },
   },
   ampm: {
@@ -21,13 +28,29 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: 300,
   },
   date: {
+    marginTop: '-1.5rem',
     fontSize: '2rem',
     fontWeight: 300,
     [theme.breakpoints.down('sm')]: {
+      marginTop: '-1rem',
       fontSize: '1.5rem',
     },
   },
 }));
+
+const greetingVariants = {
+  initial: {
+    opacity: 1,
+    x: '-10px',
+  },
+  animate: {
+    opacity: 0,
+    x: '0',
+    transition: {
+      duration: 1.5,
+    },
+  },
+};
 
 const clockVariants = {
   initial: {
@@ -90,13 +113,13 @@ export default function Clock() {
   }, [seconds, date, ampm]);
 
   return (
-    <>
-      {seconds !== null && (
-        <motion.div
-          className={classes.container}
-          initial="initial"
-          animate="animate"
-        >
+    <motion.div
+      className={classes.container}
+      initial="initial"
+      animate="animate"
+    >
+      {seconds !== null ? (
+        <>
           <motion.time className={classes.time} variants={clockVariants}>
             {`${appendZero(hours)}:${appendZero(minutes)}:${appendZero(
               seconds
@@ -108,8 +131,12 @@ export default function Clock() {
           <motion.time className={classes.date} variants={dateVariants}>
             {date}
           </motion.time>
-        </motion.div>
+        </>
+      ) : (
+        <motion.h1 className={classes.greeting} variants={greetingVariants}>
+          Good Morning!
+        </motion.h1>
       )}
-    </>
+    </motion.div>
   );
 }
--- src/components/Wallpaper.jsx
+++ src/components/Wallpaper.jsx
@@ -2,21 +2,18 @@ import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { motion } from 'framer-motion';
 import { useUnsplash } from 'context/useUnsplash';
-import photo from 'photo.jpg';
 
 const useStyles = makeStyles({
   container: (props) => ({
     position: 'relative',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
+    display: 'grid',
+    gridTemplateRows: '5vh 90vh 5vh',
     alignItems: 'center',
     width: '100vw',
     height: '100vh',
     background: `url(${props.photoUrl}) no-repeat center center/cover`,
     textAlign: 'center',
-    zIndex: -2,
-    '&::after': {
+    '&::before': {
       content: '""',
       position: 'absolute',
       top: 0,
@@ -25,8 +22,7 @@ const useStyles = makeStyles({
       bottom: 0,
       width: '100%',
       height: '100%',
-      backgroundColor: 'rgba(0, 0, 0, 0.4)',
-      zIndex: -1,
+      backgroundColor: 'rgba(0, 0, 0, 0.2)',
     },
   }),
 });
@@ -36,7 +32,7 @@ export default function Wallpaper({ children }) {
   const classes = useStyles({ photoUrl });
 
   useEffect(() => {
-    getRandomPhoto('dog');
+    getRandomPhoto('icecream');
 
     // eslint-disable-next-line
   }, []);
